ef13b58eeaa0c83b7e55f9c615a1f0d481341e71
Nishukvgce <nishm@Nishmitha_s.com>
2024-05-18 12:08:22 +0530

    voting

--- voting.java
+++ voting.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class voting {
+import javax.sound.midi.Soundbank;
+
+public class voting {
+    public static void main(String[] args){
+        int myAge = 25;
+        int votingAge = 18;
+        if(myAge >= votingAge) {
+            System.out.println("old is enough to vote");
+        }
+        else{
+            System.out.println("not have the rights to vote");
+        }
+    }
 }
