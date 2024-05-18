3b09232a1bef4fb40c9de4ad90151e6473b75497
Nishukvgce <nishm@Nishmitha_s.com>
2024-05-18 12:06:46 +0530

    time

---
+++ time.java
@@ -0,0 +1,9 @@
+public class time {
+    public static void main(String[] args)
+    {
+        int time = 20;
+        String result = (time < 20) ? "Good day" : "Bad day";
+
+        System.out.println(result);
+    }
+}
